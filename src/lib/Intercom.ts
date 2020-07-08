a3620ad052dc7859cd153ff52c8df27b93615c0c
Roman Nikitin <roma@nikitin.me>
2020-07-09 00:10:12 +0300

    fix

--- src/lib/Intercom.ts
+++ src/lib/Intercom.ts
@@ -1,6 +1,6 @@
-import * as I from "capacitor-intercom"
+import * as I from '@capacitor-community/intercom'
 
-import { Plugins } from "@capacitor/core"
+import { Plugins } from '@capacitor/core'
 
 export const PushNotifications = Plugins.PushNotifications
 export const Intercom = new I.Intercom()
